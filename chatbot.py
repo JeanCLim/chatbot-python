b432b2e9960e4748d35cbaf71242bfb84c0ac420
JeanCLIM <jeancarlosls166@gmail.com>
2026-06-29 17:53:00 -0300

    implementando classes

--- chatbot.py
+++ chatbot.py
@@ -1,15 +1,40 @@
-from funções import *
+class Chatbot():
+    def __init__(self, nome):
+        self.nome = nome
+        self.conhecidos = ['Will','João']
+        self.historico = []
 
-print('Olá, qual o seu nome?')
-nome = pegaOnome(resposta())
-resp = respondeNome(nome)
-print(resp)
+    def escuta(self):
+        frase = input('>: ')
+        frase = frase.lower()
+        frase = frase.replace('é','eh')
+        return frase
+        pass
 
-while True:
-    resp = resposta()
-    if resp == 'tchau':
-        break
-    else:
-        print('Digite outra coisa')
+    def pensa(self, frase):
+        if frase == ('oi'):
+            return 'Olá, qual seu nome?'
+        if self.historico[-1] == 'Olá, qual seu nome?':
+            nome = self.pegaNome(frase)
+            resp = self.respondeNome(nome)
+            return resp
+        return 'Não entendi!'
 
-print('Tchau tchau!')
+    def pegaNome(self, nome):
+        if 'o meu nome eh ' in nome:
+            nome = nome[14:]
+            
+        nome = nome.title()
+        return nome
+
+    def respondeNome(self, nome):
+        if nome in self.conhecidos:
+            frase = 'Eaew '
+        else:
+            frase = 'Muito prazer '
+
+        return frase+nome
+
+    def fala(self, frase):
+        print(frase)
+        self.historico.append(frase)
---
+++ main.py
@@ -0,0 +1,10 @@
+from Chatbot import Chatbot
+
+Bot = Chatbot("Felipe")
+while True:
+    frase = Bot.escuta()
+    resp = Bot.pensa(frase)
+    Bot.fala(resp)
+    
+    if resp == 'tchau':
+        break
